87a6ce840d4134e55f479b465a97f9148a510791
Paul Mooney <paul@gmst.ie>
2023-08-01 18:13:21 +0100

    Adjusted to simulate stored XSS attack

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,11 +1,6 @@
 export default function handler(req, res) {
     if (req.method === 'POST') {
-        // Process a POST request
-        const {
-            query
-        } = req.body;
-        // Return the query directly without sanitizing it
-        res.status(200).send(query);
+        res.status(200).send('<img src="x" onerror="alert(\'Stored XSS attack\')">');
     } else {
         // Handle any other HTTP method
         res.status(405).end(`Method ${req.method} Not Allowed`);
