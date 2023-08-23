e49dac78aa2a7af9972ea40f678f6628d88e1236
Paul Mooney <paul@gmst.ie>
2023-08-23 11:42:12 +0100

    Removed unneccesary function

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 
 // Specify the CORS configuration
-var corsOptions = {
+let corsOptions = {
     origin: 'http://localhost:3000', // This should be your client-side URL
     optionsSuccessStatus: 200,
     credentials: true // This allows the session cookie to be sent back and forth
@@ -14,16 +14,6 @@ var corsOptions = {
 
 // Use the CORS middleware, passing in the options
 app.use(cors(corsOptions));
-
-// Route to set a cookie
-app.get('/set-cookie', (req, res) => {
-    res.cookie('myCookie', 'Hello World', {
-        httpOnly: true,
-        sameSite: 'none',
-        secure: true
-    });
-    res.send('Cookie has been set!');
-});
 
 // Route to read cookies
 app.get('/read-cookie', async (req, res) => {
